1ba3f42468df8d3bd2b1b1db3473aae5561f1b7d
penteract <tcathartburn@gmail.com>
2012-10-14 20:38:08 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+# !/usr/bin/env python
+
+import os
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp import util
+#from django.utils import simplejson
+
+class MainPage(webapp.RequestHandler):
+    """ Renders the main template."""
+    def get(self):
+        template_values = { 'title':'AJAX Add (via GET)', }
+        path = os.path.join(os.path.dirname(__file__), "canvas.html")
+        self.response.out.write(template.render(path, template_values))
+
+class RPCHandler(webapp.RequestHandler):
+    """ Allows the functions defined in the RPCMethods class to be RPCed."""
+
+    def get(self):
+        func = None
+
+        action = self.request.get('action')
+        if action=="Add":
+            try:
+                num1 = int(self.request.get('arg1'))
+                num2 = int(self.request.get('arg2'))
+            except:
+                self.error(400)#invalid input
+                return
+            self.response.out.write(str(num1+num2))
+            return
+        
+        self.error(403) # access denied
+
+
+  
+app = webapp.WSGIApplication([
+    ('/', MainPage),
+    ('/rpc', RPCHandler),
+     ], debug=True)
+        
